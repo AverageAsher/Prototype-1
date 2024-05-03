d4f4e5a605a454e5dfc0660352d028555e015703
Someidiotter <anullowo@gmail.com>
2024-05-03 15:07:38 -0500

    YUH

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public object Public { get; private set; } 
+    public float speed = 20;
+    public float horizontalInput;
+    public float forwardInput;
+    public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+       
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+       
     }
 }
